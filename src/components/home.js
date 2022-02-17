052c7ec0493589497ab524510f63240d0a833155
Lidianys <lidianysrodriguez96@gmail.com>
2022-02-17 12:31:47 -0300

    estructura de home.js y register.js y css

---
+++ src/components/home.js
@@ -0,0 +1,69 @@
+// aqui exportaras las funciones que necesites
+export const home = () => {
+  // aqui tu codigo del formulario
+  // creacion de logo y reseña
+  const divImg = document.createElement('div');
+  divImg.classList.add('divImg');
+  const imgLogo = document.createElement('img');
+  imgLogo.setAttribute('src', './img/pruebaDeLogo.png');
+  imgLogo.classList.add('imgLogo');
+  const review = document.createElement('h1');
+  review.classList.add('review');
+  review.innerText = 'Red informativa para migrantes y futuros migrantes en Chile';
+  // creacion de dos articulos y sus clases
+  // insertar en el articulo img y reseña
+  const asideLogo = document.createElement('aside');
+  divImg.appendChild(imgLogo);
+  asideLogo.appendChild(divImg);
+  asideLogo.appendChild(review);
+  // insertar en el articulo los inputs y botones
+  // insertar en el elemento con id container los dos articulos
+  const headerPart = document.createElement('header');
+  headerPart.classList.add('header');
+  const footPart = document.createElement('footer');
+  footPart.classList.add('footer');
+  footPart.innerText = 'Todos los derechos reservados 2022. Propiedad de Cyntia, Norkis y Lidianys';
+  const container = document.createElement('div');
+  // creacion de los input y sus atributos
+  const inputEmail = document.createElement('input');
+  const inputPassword = document.createElement('input');
+  inputEmail.setAttribute('type', 'email');
+  inputPassword.setAttribute('type', 'password');
+  inputEmail.setAttribute('placeholder', ' Email');
+  inputPassword.setAttribute('placeholder', ' Contraseña');
+  inputEmail.classList.add('inputEmail');
+  inputPassword.classList.add('inputPassword');
+  // creacion de button para iniciar sesion
+  const button = document.createElement('button');
+  button.innerText = 'Iniciar Sesión';
+  button.classList.add('iniciarSesion');
+  // creacion de link y seteo del atributo
+  const a = document.createElement('a');
+  a.setAttribute('href', '');
+  a.innerText = '¿Olvidaste tu contraseña?';
+  // creacion de boton
+  const buttonGoogle = document.createElement('button');
+  buttonGoogle.innerText = 'Continuar con Google';
+  buttonGoogle.classList.add('googleButton');
+  // creacion de link y seteo del atributo
+  const aRegister = document.createElement('a');
+  aRegister.classList.add('aregister');
+  aRegister.setAttribute('href', '');
+  aRegister.innerText = 'Registrarse';
+  const containerForm = document.createElement('div');
+  containerForm.classList.add('containerForm');
+  const generalContainer = document.getElementById('generalContainer');
+  generalContainer.classList.add('generalContainer');
+  containerForm.appendChild(inputEmail);
+  containerForm.appendChild(inputPassword);
+  containerForm.appendChild(button);
+  containerForm.appendChild(a);
+  containerForm.appendChild(aRegister);
+  containerForm.appendChild(buttonGoogle);
+  container.appendChild(containerForm);
+  generalContainer.appendChild(headerPart);
+  generalContainer.appendChild(asideLogo);
+  generalContainer.appendChild(container);
+  generalContainer.appendChild(footPart);
+  return generalContainer;
+};
