2efa607e55e97e1bb9a156f9b9da34539f9e395b
CyntiaDalesio <cyntiam.135@gmail.com>
2022-02-20 21:25:12 -0300

    funcionalidad registrar y loguearse

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,66 +1,44 @@
 // Import the functions you need from the SDKs you need
-// import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js';
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js';
 // // import { firestore } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-firestore.js";
 
-// import {
-//   collection,
-//   addDoc,
-//   getFirestore,
-//   onSnapshot,
-//   query,
-//   orderBy,
-//   doc,
-//   deleteDoc,
-//   updateDoc,
-//   getDoc,
-//   arrayRemove,
-//   arrayUnion,
-// } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-firestore.js';
+import {
+  getFirestore,
 
-// import {
-//   getAuth,
-//   createUserWithEmailAndPassword,
-//   signInWithEmailAndPassword,
-//   GoogleAuthProvider,
-//   signInWithPopup,
-//   singIn,
-//   sendEmailVerification,
-//   onAuthStateChanged,
-//   updateProfile,
-// } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js';
+} from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-firestore.js';
 
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyANmHTIUeRiBP9UGcDmCJS82HoT68JwM7E',
-//   authDomain: 'social-network-migraruni-950b7.firebaseapp.com',
-//   projectId: 'social-network-migraruni-950b7',
-//   storageBucket: 'social-network-migraruni-950b7.appspot.com',
-//   messagingSenderId: '185329185729',
-//   appId: '1:185329185729:web:b6929fde86929fe2f9f540',
-// };
+import {
+  getAuth,
+  signOut,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  sendEmailVerification,
+  updateProfile,
+} from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyANmHTIUeRiBP9UGcDmCJS82HoT68JwM7E',
+  authDomain: 'social-network-migraruni-950b7.firebaseapp.com',
+  projectId: 'social-network-migraruni-950b7',
+  storageBucket: 'social-network-migraruni-950b7.appspot.com',
+  messagingSenderId: '185329185729',
+  appId: '1:185329185729:web:b6929fde86929fe2f9f540',
+};
+
+const app = initializeApp(firebaseConfig);
 
 // // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// export const auth = getAuth(app);
+
+export const auth = getAuth(app);
 // const db = getFirestore(app);
 // const provider = new GoogleAuthProvider(app);
 // export const user = auth.currentUser;
 
-// const emailCheck = () => {
-//   sendEmailVerification(auth.currentUser)
-//     .then(() => {
-//       // Email verification sent!
-//       console.log('Correo enviado');
-//       alert('Hemos enviado un correo de verificación para validar tu cuenta');
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     });
-// };
-
-// // Funcion para registrarte
+// Funcion para registrarte
 // export const signUp = (email, password, name) => {
 //   createUserWithEmailAndPassword(auth, email, password)
 //     .then((userCredential) => {
@@ -83,19 +61,19 @@
 //       // ..
 //     });
 
-//   // Envía un mensaje de verificación a un usuario
-// //   const emailCheck = () => {
-// //     sendEmailVerification(auth.currentUser)
-// //       .then(() => {
-// //         // Email verification sent!
-// //         console.log('Correo enviado');
-// //         alert('Hemos enviado un correo de verificación para validar tu cuenta');
-// //       })
-// //       .catch((error) => {
-// //         console.log(error);
-// //       });
-// //   };
-// // };
+// Envía un mensaje de verificación a un usuario
+//   const emailCheck = () => {
+//     sendEmailVerification(auth.currentUser)
+//       .then(() => {
+//         // Email verification sent!
+//         console.log('Correo enviado');
+//         alert('Hemos enviado un correo de verificación para validar tu cuenta');
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   };
+// };
 
 // // Iniciar sesión con correo registrado
 // export const singIn = (emailRegister, passwordRegister) => {
@@ -241,3 +219,48 @@
 //   }
 // };
 // }
+
+export const eventsRegister = () => {
+  const signupForm = document.querySelector('.signup');
+  signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = signupForm.email.value;
+    const password = signupForm.password.value;
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((cred) => {
+        console.log('User created: ', cred.user);
+        signupForm.reset();
+      }).catch((err) => {
+        console.log(err.message);
+        alert(err.message);
+      });
+  });
+};
+export const logout = () => {
+  const logoutButton = document.querySelector('.logout');
+  logoutButton.addEventListener('click', () => {
+    signOut(auth)
+      .then(() => {
+        console.log('el usuario salió');
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  });
+};
+
+export const login = () => {
+  const loginForm = document.querySelector('.login');
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = loginForm.email.value;
+    const password = loginForm.password.value;
+    signInWithEmailAndPassword(auth, email, password)
+      .then((cred) => {
+        console.log('user logged in:', cred.user);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  });
+};
--- src/lib/router.js
+++ src/lib/router.js
@@ -1,5 +1,6 @@
 import { home } from '../components/home.js';
 import { register } from '../components/register.js';
+import { eventsRegister, login } from './index.js';
 // import { wallTemplate } from '../templates/wallpage.js';
 
 export const router = (hash) => {
@@ -7,10 +8,13 @@ export const router = (hash) => {
   rootBox.innerHTML = '';
   if (hash === '#/' || hash === '/' || hash === '#' || hash === '') {
     rootBox.appendChild(home());
+    login();
   } else if (hash === '#/home') {
     rootBox.appendChild(home());
+    login();
   } else if (hash === '#/register') {
     rootBox.appendChild(register());
+    eventsRegister();
     //   } else if (hash === '#/wallpage') {
     //     rootBox.appendChild(wallTemplate());
     //   } else if (hash === '#/showpost') {
