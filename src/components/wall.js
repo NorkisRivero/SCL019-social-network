faa6145fbcac953a557c7aea92ec57ac5b72f9af
CyntiaDalesio <cyntiam.135@gmail.com>
2022-02-22 16:49:32 -0300

    wall header

--- src/components/wall.js
+++ src/components/wall.js
@@ -1,21 +1,24 @@
 import { logout } from '../lib/index.js';
+
 export const wall = () => {
-const wall = `
-<header>
-<button >Inicio</button>
-<button>Muro</button>
-<button>Agregar</button>
-<button class="logout" id="logout">Salir</button>
+  const walls = `
+<header class="headerWall">
+<button class = "logo"><img src="./img/pruebaDeLogo.png"></button>
+<button class = "addComent"><img src="./img/agregar.png"></button>
+<button class = "home"><img src="./img/home.png"></button>
+<button class = "perfile"><img src="./img/perfil.png"></button>
+<button class="logout" id="logout"><img src="./img/salir.png"></button>
 </header>
 `;
-const divHeader= document.createElement('div');
-divHeader.innerHTML = wall;
-const buttonLogout = divHeader.querySelector('.logout');
-if(buttonLogout){buttonLogout.addEventListener('click', () =>{
-    logout();
-})
-};
+  const divHeader = document.createElement('div');
+  divHeader.classList.add('divHeader');
+  divHeader.innerHTML = walls;
+  const buttonLogout = divHeader.querySelector('.logout');
+  if (buttonLogout) {
+    buttonLogout.addEventListener('click', () => {
+      logout();
+    });
+  }
 
-
-return divHeader;
+  return divHeader;
 };
