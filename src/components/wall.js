3458d20fae35814e7fbbff3d119c86ef6f4e9b19
Lidianys <lidianysrodriguez96@gmail.com>
2022-03-09 12:50:36 -0300

    arreglos del css

--- src/components/wall.js
+++ src/components/wall.js
@@ -5,12 +5,11 @@ import { post } from './post.js';
 export const wall = () => {
   const walls = `
 <header class="headerWall">
-<div class = "logo"><img src="./img/pruebaDeLogo.png" class= "imgLogo"></div>
-<button class = "addComent"><img src="./img/agregar.png"></button>
-<button class = "home"><img src="./img/home.png"></button>
-<button class = "perfile"><img src="./img/perfil.png"></button>
-<button class="logout" id="logout"><img src="./img/salir.png"></button>
-</header>
+<div class = "logo"><img src="./img/pruebaDeLogo.png" class= "imgLogoPost"> </div>
+<button class = "addComent"><img src="./img/agregar.png"> <span class="tooltiptext">Agregar comentarios</span></button>
+<button class = "home"><img src="./img/home.png"><span class="tooltiptext1">Ver todos los post</span></button>
+<button class = "perfile"><img src="./img/perfil.png"><span class="tooltiptext2">Ver mis post</span></button>
+<button class="logout" id="logout"><img src="./img/salir.png"><span class="tooltiptext3">Salir de la sesion</span>
 <section id = "allPost" class = "allPost">
 </section>
 `;
