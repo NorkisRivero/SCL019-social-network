54162866cddcc90343c7a4f1297c24aeef953086
CyntiaDalesio <cyntiam.135@gmail.com>
2022-03-09 16:55:45 -0300

    editpost header arreglado

--- src/components/editPost.js
+++ src/components/editPost.js
@@ -3,25 +3,25 @@ import { editDeletePost } from '../lib/index.js';
 
 export const editPost = () => {
   const edit = `
-<header class="headerWall1">
-<div class = "logo1"><img src="./img/pruebaDeLogo.png" class= "imgLogo"></div>
-<button class = "home1"><img src="./img/home.png"></button>
-<button class = "perfile1"><img src="./img/perfil.png"></button>
-<button class="logout1" id="logout"><img src="./img/salir.png"></button>
-</header>
+<header class="headerWall">
+<div class = "logo"><img src="./img/pruebaDeLogo.png" class= "imgLogo"></div>
+<button class="home"><img src="./img/home.png"><span class="tooltiptext1">Ver todos los post</span></button>
+<button class="perfile"><img src="./img/perfil.png"><span class="tooltiptext2">Ver mis post</span></button>
+<button class="logout" id="logout"><img src="./img/salir.png"><span class="tooltiptext3">Salir de la sesion</span>
+
 <section id = "allPost1" class = "allPost1">
 </section>
 `;
   const divHeader = document.createElement('div');
   divHeader.classList.add('divHeader1');
   divHeader.innerHTML = edit;
-  const buttonHome = divHeader.querySelector('.home1');
+  const buttonHome = divHeader.querySelector('.home');
   if (buttonHome) {
     buttonHome.addEventListener('click', () => {
       window.location.hash = '#/wall';
     });
   }
-  const buttonLogout = divHeader.querySelector('.logout1');
+  const buttonLogout = divHeader.querySelector('.logout');
   if (buttonLogout) {
     buttonLogout.addEventListener('click', (e) => {
       e.preventDefault();
